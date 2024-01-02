ab8dda40c219de8f610a61b8f7bee5f98ff5297d
Ahlem Mezemate <103861671+4hlem@users.noreply.github.com>
2024-01-02 17:01:03 +0100

    added chatbot urls

--- projet_amlak/urls.py
+++ projet_amlak/urls.py
@@ -8,8 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    path('chatbot/', include('chatbot.urls')),
 
 
 ]
